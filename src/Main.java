cc6ebc72c964c4686a779fe3737d887cf9dc39d9
Stepanov <websafari@yandex.ru>
2020-03-21 18:29:49 +0300

    add 1,2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Bash");
         System.out.println("Hello Git Bash2");
+        System.out.println("1");
+        System.out.println("2");
     }
 }
